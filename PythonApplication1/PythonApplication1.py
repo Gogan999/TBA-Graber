00669d49637671cafc0579ec0cebd304c06c1685
Logan <Logan@DESKTOP-Q14A6KM>
2018-07-30 00:48:12 -0500

    Gogals

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -30,3 +30,19 @@ while ( n >= 0 ): #works backwards to 0 listing off all team numbers
     print(teams[n])
     n = n - 1
 
+
+""" Goals
+Take match data and dump to regonal folder
+    https://www.thebluealliance.com/api/v3/event/2018mndu/matches
+Figure pull match numbers then look up in regonal folder?
+    https://www.thebluealliance.com/api/v3/team/frc5690/event/2018mndu/matches
+take the data and save as a json for sepecific team
+
+Flieing ideas
+    Data_Dump
+        Teams
+            5690
+            254
+        Events
+            2018mndu
+"""
