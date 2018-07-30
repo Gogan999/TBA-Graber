bbf9ee588961a1657c0913b384266363d75eaca5
Logan <Logan@DESKTOP-Q14A6KM>
2018-07-30 00:26:59 -0500

    Added basic coments and renamed some stuff

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,9 +1,9 @@
-import requests
-import json
+import requests #stuff for https data retreaval
+import json #Cuz the datas steralized 
 
-print("This line will be printed.")
+print("This line will hopefully be printed.") #cuz idk what im doing
 
-x = 1
+x = 1 #i copyed and pasted this bit 
 if x == 1:
     # indented four spaces
     print("x is 1.")
@@ -12,20 +12,21 @@ while (x <= 10):
     print("Shit is counting: ", x)
     x = x + 1
 
-status = {'X-TBA-Auth-Key':'ir3K1D1jaFLVPteNNBo7Q3CeZocBaEU8kIygdLmBMqHOq7fthFiwffAbi5s25NpO'}
-r = requests.get('https://www.thebluealliance.com/api/v3/event/2018mndu/teams/simple', params=status)
+status = {'X-TBA-Auth-Key':'ir3K1D1jaFLVPteNNBo7Q3CeZocBaEU8kIygdLmBMqHOq7fthFiwffAbi5s25NpO'} #seting up auth key
+r = requests.get('https://www.thebluealliance.com/api/v3/event/2018mndu/teams/simple', params=status) #requesting info from TBA
 print(r)
 print(r.text)
-stuffs = json.loads(r.text)
+stuffs = json.loads(r.text) #takes the json and de steralises it into a list
 
-teams = []
-for stuff in stuffs:
-    teams.append(stuff["team_number"])
+teams = [] #soon to be list of team numbers
+for team in stuffs:                   #sorts through stuffs deviding it up into team     
+    teams.append(team["team_number"]) #adds team number to the end of the list teams
 
-n = len(teams) - 1 
+n = len(teams) - 1 #gets totel number of team 
 
-print(teams[0])
+print(teams[0]) #Prints team 0 in list
 
-while ( n >= 0 ):
+while ( n >= 0 ): #works backwards to 0 listing off all team numbers 
     print(teams[n])
     n = n - 1
+
