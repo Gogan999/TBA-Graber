14b35089c78b1774d7cf8b48639db7620487698e
Hunter <huntox1@hotmail.com>
2018-07-30 18:24:18 -0500

    this is the regionals list update

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -20,7 +20,7 @@ stuffs = json.loads(r.text) #takes the json and de steralises it into a list
 
 teams = [] #soon to be list of team numbers
 for team in stuffs:                   #sorts through stuffs deviding it up into team     
-    teams.append(team["team_number"]) #adds team number to the end of the list teams
+    teams.append(team["key"]) #adds team number to the end of the list teams
 
 n = len(teams) - 1 #gets total number of teams in index 0
 
@@ -30,7 +30,19 @@ while ( n >= 0 ): #works backwards to 0 listing off all team numbers
     print(teams[n])
     n = n - 1
 
+n = len(teams) - 1 #resetting the team list for my while loop iteration
 
+regionals = [] #making a list of regionals attended by the teams in the "teams" list
+tempteamjsontext = 0
+while ( n >= 0 ): #as long as we have more than one team left in out list counter 
+    teamiterate = teams[n] #use that teams index to get the team_key  
+    linkR = 'https://www.thebluealliance.com/api/v3/team/'+ str(teamiterate) + '/events/2018'
+    tempteamjsontext = requests.get(linkR, params=status) #then assign the json file we retrive for that team to "tempteamjsontext"
+    regionaltext = json.loads(tempteamjsontext.text) #change the json file we get to a readable list
+    for i in regionaltext: #now we iterate through this list
+        regionals.append("2018" + i["event_code"]) #and add every event code to the list "regionals"
+    n = n - 1 #now change our list counter down one and do this for each subsequent team until it gets to zero
+#the list "regionals" now has all of the events attended by all of the teams in the regional specified in the original stuffs link
 """ Goals
 Multy thread program?
 Take match data and dump to regonal folder
