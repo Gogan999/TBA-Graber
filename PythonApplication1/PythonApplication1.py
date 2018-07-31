21a4921d3d4283a56b380d73ae86b2d437e74034
Logan <Logan@DESKTOP-Q14A6KM>
2018-07-31 02:05:51 -0500

    Added some status indicators

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -17,12 +17,13 @@ for team in stuffs:                   #sorts through stuffs deviding it up into 
 
 n = len(teams) - 1 #gets total number of teams in index 0
 
-print(teams[0]) #Prints team 0 in list
-
 while ( n >= 0 ): #works backwards to 0 listing off all team numbers 
     print(teams[n])
     n = n - 1
 
+if not os.path.exists('data/events/last_updated.json'): #Checks that the file path exists before writing to it
+        with open('data/events/' + 'last_updated' + '.json', 'w') as outfile: #creats json file and puts the data in it
+            json.dump({'date':'null'}, outfile)
 with open('data/events/last_updated.json', 'r') as lastUpdate:
     date = json.load(lastUpdate)
     
@@ -30,13 +31,17 @@ if (date["date"] != str(datetime.date.today())): #Checks to see if last updated 
     n = len(teams) - 1 #resetting the team list for my while loop iteration
     
     regionals = [] #making a list of regionals attended by the teams in the "teams" list
+    print('Gathering Data')
     while ( n >= 0 ): #as long as we have more than one team left in out list counter 
-        teamiterate = teams[n] #use that teams index to get the team_key  
+        teamiterate = teams[n] #use that teams index to get the team_key
+        print(teamiterate, end='')
         linkR = 'https://www.thebluealliance.com/api/v3/team/'+ str(teamiterate) + '/events/2018'
         tempteamjsontext = requests.get(linkR, params=status) #then assign the json file we retrive for that team to "tempteamjsontext"
         regionaltext = json.loads(tempteamjsontext.text) #change the json file we get to a readable list
         for i in regionaltext: #now we iterate through this list
             regionals.append("2018" + i["event_code"]) #and add every event code to the list "regionals"
+            print('---', end='')
+        print('')
         n = n - 1 #now change our list counter down one and do this for each subsequent team until it gets to zero
     regionals=list(set(regionals)) #this gets rid of duplicates in the list "regionals"
     #the list "regionals" now has all of the events attended by all of the teams in the regional specified in the original stuffs link
@@ -44,8 +49,6 @@ if (date["date"] != str(datetime.date.today())): #Checks to see if last updated 
 
 """ Goals
 Multy thread program?
-Take match data and dump to regonal folder
-    https://www.thebluealliance.com/api/v3/event/2018mndu/matches
 Figure pull match numbers then look up in regonal folder?
     https://www.thebluealliance.com/api/v3/team/frc5690/event/2018mndu/matches
 take the data and save as a json for sepecific team
@@ -54,7 +57,11 @@ Flieing ideas
     Data_Dump
         Teams
             5690
+                match data
+                team evaluation
             254
+                match data
+                team evaluation
         Events
             2018mndu
 """
@@ -62,14 +69,17 @@ Flieing ideas
 def eventMatchPuller(eventKey): #Function for requesting event matches and then writing them to a file path
     evData = requests.get(TBA + '/event/' + eventKey + '/matches', params=status)
     evDataJson = json.loads(evData.text)
-    if not os.path.exists('data/events/'): #Checks that the file path exists before writing to it
-        os.makedirs('data/events/')
+    print(' --- ', end='')
     with open('data/events/' + eventKey + '.json', 'w') as outfile:
         json.dump(evDataJson, outfile)
+        print('Dumped')
 
 if (date["date"] != str(datetime.date.today())): #Checks to see if last updated json was created
+    print('Writing Data')
     for i in regionals:
+        print(i, end='')
         eventMatchPuller(i)
+    print('All Data Aquired')
 
 if (date["date"] != str(datetime.date.today())): #Checks to see if last updated json was created
     with open('data/events/' + 'last_updated' + '.json', 'w') as outfile: #creats json file and puts the data in it
